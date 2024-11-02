41edc0dba3a835a2ed536753f7b355a068208e84
Tom Ding <tomding.biostat@gmail.com>
2024-11-02 09:41:12 +0800

    Initial commit

--- R/TrialPred.TwoArm.R
+++ R/TrialPred.TwoArm.R
@@ -1,37 +1,55 @@
 
+#' Title
+#'
+#' @param N.0
+#' @param N.1
+#' @param ratio
+#' @param d
+#' @param l
+#' @param gamma.c
+#' @param alpha0.t
+#' @param nu0.t
+#' @param alpha1.t
+#' @param nu1.t
+#' @param s
+#' @param m
+#'
+#' @return
+#' @export
+#'
+#' @examples
 TrialPred.TwoArm <- function(
                                 N.0=NULL,       # Number of subjects plan to be enrolled in group 1
                                 N.1=NULL,       # Number of subjects plan to be enrolled in group 2
                                 ratio=NULL,     # N.1 / N.0
                                 d=NULL,         # expected number of events observed at l
-                                l=NULL,         # observation time  
+                                l=NULL,         # observation time
                                  gamma.c=NULL,  # parameters of a weibull censoring distribution for both groups
                                 alpha0.t=NULL,  # shape parameter of a weibull survival distribution for group 1
                                 nu0.t=NULL,     # scale parameter of a weibull survival distribution for group 1
                                 alpha1.t=NULL,  # shape parameters of a weibull survival distribution for group 2
                                 nu1.t=NULL,     # scale parameter of a weibull survival distribution for group 2
                                 s=NULL,         # enrollment period time
-                                m=NULL,         # maximum follow-up time for a subject
-                                
-                                
+                                m=NULL         # maximum follow-up time for a subject
+
+
 ){
-  
-  if(is.null(d)){
+
+  if( is.null(d) ){
     return(NumEventsSubTwoArm(N.0=N.0,N.1=N.1,alpha0.t = alpha0.t,nu0.t=nu0.t,alpha1.t=alpha1.t,nu1.t=nu1.t,gamma.c=gamma.c,s=s,m=m,l=l))
   }
-  
-  else if(is.null(N.0) & is.null(N.1)){
+
+  else if( is.null(N.0) & is.null(N.1) ){
     return(NumEventsSubTwoArm(ratio=ratio,d=d,alpha0.t = alpha0.t,nu0.t=nu0.t,alpha1.t=alpha1.t,nu1.t=nu1.t,gamma.c=gamma.c,s=s,m=m,l=l))
   }
-  
-  else if(is.null(l)){
+
+  else if( is.null(l) ){
     return(ObsTimeTwoArm(N.0=N.0,N.1=N.1,d=d,alpha0.t = alpha0.t,nu0.t=nu0.t,alpha1.t=alpha1.t,nu1.t=nu1.t,gamma.c=gamma.c,s=s,m=m))
   }
-  
-  else if(is.null(gamma.c)){
+
+  else if( is.null(gamma.c) ){
     return(CensTimeTwoArm(N.0=N.0,N.1=N.1,d=d,alpha0.t = alpha0.t,nu0.t=nu0.t,alpha1.t=alpha1.t,nu1.t=nu1.t,s=s,m=m,l=l))
   }
 }
 
 
-  
