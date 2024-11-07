a5f90fa94d27df029b65dd5e3687bd1ff9819075
Tom Ding <tomding.biostat@gmail.com>
2024-11-07 14:22:05 +0800

    1107

--- R/TrialPred.TwoArm.R
+++ R/TrialPred.TwoArm.R
@@ -1,24 +1,25 @@
 
 #' Title
 #'
-#' @param N.0 The number of subjects plan to be enrolled in control arm
-#' @param N.1 The number of subjects plan to be enrolled in experimental arm
-#' @param ratio The randomization ratio between two arms: N.1 / N.0
-#' @param d The expected number of events observed at time l
-#' @param l The observation time
-#' @param gamma.c The parameter of the exponential distribution of censoring time
-#' @param alpha0.t The shape parameter of weibull survival distribution for control arm
-#' @param nu0.t The scale parameter of weibull survival distribution for control arm
-#' @param HR The hazard ratio of experimental group over control group
-#' @param alpha1.t The shape parameters of weibull survival distribution for experimental arm
-#' @param nu1.t The scale parameter of a weibull survival distribution for control arm
-#' @param s  The enrollment time
-#' @param m The maximum follow-up time for a subject
+#' @param N.0 number of subjects plan to be enrolled in control arm
+#' @param N.1 number of subjects plan to be enrolled in experimental arm
+#' @param ratio randomization ratio between two arms: N.1 / N.0
+#' @param d expected number of events observed at time l
+#' @param l observation time
+#' @param gamma.c parameter of the exponential distribution of censoring time
+#' @param alpha0.t shape parameter of weibull survival distribution for control arm
+#' @param nu0.t scale parameter of weibull survival distribution for control arm
+#' @param HR hazard ratio of experimental group over control group
+#' @param alpha1.t shape parameters of weibull survival distribution for experimental arm
+#' @param nu1.t scale parameter of a weibull survival distribution for control arm
+#' @param s  enrollment time
+#' @param m maximum follow-up time for a subject
 #'
-#' @return ssss
+#' @return This function returns a list including all design parameters as the same with input parameters of this function. If parameter d is missing, this function will calculate l, based on other parameters. If parameter N.0 or N.1 is missing, this function will calculate N.0 or $N.1$, based on other parameters. If parameter $l$ is missing, this function will calculate $l$ based on other parameters. If parameter gamma.c is missing, this function will calculate gamma.c based on other parameters.
 #' @export
 #'
-#' @examples sssss
+#' @examples # calculate the expected number of events
+#' TrialPred.TwoArm(N.0=100,N.1=100,alpha0.t = 1,nu0.t=5,alpha1.t=2,nu1.t=4,gamma.c=1,s=5,m=4,l=6)
 TrialPred.TwoArm <- function(
                                 N.0=NULL,
                                 N.1=NULL,
